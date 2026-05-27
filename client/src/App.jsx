86a3393f94882920450838aabd00e8a641b4e826
김민석 <kms53311902@gmail.com>
2026-05-27 22:33:18 +0900

    feat: /write 라우트 추가 및 메인 화면 글쓰기 버튼 연결

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import DetailPage from './pages/DetailPage'
+import WritePage from './pages/WritePage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/rides/:id" element={<DetailPage />} />
+        <Route path="/write" element={<WritePage />} />
       </Routes>
     </BrowserRouter>
   )
