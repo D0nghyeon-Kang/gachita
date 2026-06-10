02bbdd18c908df87c6f4da4dc96c89cd5f031401
Donghyeon Kang <kai2312119@gmail.com>
2026-06-10 14:19:15 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,15 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// CORS 설정 - 배포 환경 포함
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://gachita-client.vercel.app',
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // 라우터 등록
@@ -13,8 +21,10 @@ app.use('/api/applications', require('./routes/applications'));
 app.use('/api/users',        require('./routes/users'));
 app.use('/api/reviews',      require('./routes/reviews'));
 
+// 헬스 체크
 app.get('/', (req, res) => res.json({ message: 'GachiTa API 서버' }));
 
+// 404 처리
 app.use((req, res) => res.status(404).json({ error: '존재하지 않는 API입니다.' }));
 
 module.exports = app;
