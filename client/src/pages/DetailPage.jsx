425dceae2ee6ffb55bc1b802f65dc4909d083f1b
김민석 <kms53311902@gmail.com>
2026-06-11 18:51:03 +0900

    fix: 상세 화면 평점 → 매너 온도 및 아이콘 수정

--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -221,10 +221,8 @@ function DetailPage() {
             </li>
             <li className="d-flex justify-content-between align-items-center">
               <span className="text-secondary d-flex align-items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-                평점
+                🌡️
+                매너 온도
               </span>
               <StarRating rating={rating} size={15} />
             </li>
